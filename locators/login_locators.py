ba6c4e4756949721763d5d40a422d0355b36a906
UANEMESIS <mike_zone@ukr.net>
2021-11-25 23:40:30 +0200

    Update login_locators.py

--- locators/login_locators.py
+++ locators/login_locators.py
@@ -14,12 +14,3 @@ PROFILE_USERNAME = "com.socialsuperstore:id/usernameTextView"
 SIGN_OUT_BTN = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView[3]/android.view.ViewGroup[2]/android.widget.TextView"
 ACCEPT_MODAL_BTN_LOGOUT = "com.socialsuperstore:id/positiveButton"
 LOGIN_SCREEN_TITLE = "com.socialsuperstore:id/body"
-
-
-
-
-# SEARCH LOCATORS
-SEARCH_INPUT_FIELD = "com.socialsuperstore:id/searchInput"
-SELECT_SUGGESTED_ITEM_SEARCH = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[2]/android.widget.ImageView"
-FIRST_ITEM_NAME_SEARCH = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/androidx.viewpager.widget.ViewPager/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[1]"
-CLEAR_SEARCH_BTN = "com.socialsuperstore:id/searchClear"
